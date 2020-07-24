9945d5ae81e1218b11e4e6316fb7f22cda677f89
Anna Kryva <anna120100@gmail.com>
2020-07-24 16:19:05 +0300

    Added data fetching in the main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styled from 'styled-components/native';
-import {FlatList, ListRenderItem} from 'react-native';
+import {FlatList, ListRenderItem, Text} from 'react-native';
 
-import {Article} from '../generated/graphql';
+import {useArticlesQuery} from '../generated/graphql';
+import {ArticleCardType, MainScreenProps} from '../types';
+
 import {ArticleCard} from '../components/ArticleCard';
+import {LoadingSpinner} from '../components/LoadingSpinner';
+import {EmptyList} from '../components/EmptyList';
 
 const Container = styled.View`
   flex: 1;
   padding: 10px;
 `;
 
-export const MainScreen: React.FC<Props> = () => {
-  const renderItem: ListRenderItem<Article> = ({item}) => (
-    <ArticleCard article={item} />
+export const MainScreen: React.FC<MainScreenProps> = ({navigation}) => {
+  const {loading, error, data} = useArticlesQuery({
+    fetchPolicy: 'network-only',
+  });
+
+  const pressHandler = useCallback(
+    (id: string): void => {
+      navigation.navigate('Article', {id});
+    },
+    [navigation],
   );
+
+  const renderItem = useCallback<ListRenderItem<ArticleCardType>>(
+    ({item}) => <ArticleCard article={item} onPress={pressHandler} />,
+    [pressHandler],
+  );
+
+  if (error) {
+    return <Text>Error :(</Text>;
+  }
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <Container>
-      <FlatList
-        data={articles}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+      {data!.articles.length !== 0 ? (
+        <FlatList
+          data={data!.articles}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      ) : (
+        <EmptyList />
+      )}
     </Container>
   );
 };
