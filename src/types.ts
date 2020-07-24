33e3cc607df45981cd85c81b1864f9656b429217
Anna Kryva <anna120100@gmail.com>
2020-07-24 16:17:51 +0300

    Added types for screens

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,20 @@
 import {ArticlesQuery} from './generated/graphql';
+import {StackScreenProps} from '@react-navigation/stack';
 
 export type ArticleCardType = ArticlesQuery['articles'][number];
 
 export type TagType = ArticleCardType['tags'][number];
 
 export type CoverImage = ArticleCardType['cover'];
+
+type RootStackParamList = {
+  Main: undefined;
+  Article: {id: string};
+};
+
+export type ArticleScreenProps = StackScreenProps<
+  RootStackParamList,
+  'Article'
+>;
+
+export type MainScreenProps = StackScreenProps<RootStackParamList, 'Main'>;
