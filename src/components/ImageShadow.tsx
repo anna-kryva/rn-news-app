2fd16773624111b9d168f98af2c766a631411d5e
Anna Kryva <anna120100@gmail.com>
2020-07-26 22:56:31 +0300

    Fixed rerendering of the gradient

--- src/components/ImageShadow.tsx
+++ src/components/ImageShadow.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import Svg, {LinearGradient, Defs, Stop, Rect} from 'react-native-svg';
 
-interface Props {
-  width?: number;
-  height?: number;
-  children?: React.ReactChild;
-}
-
-export const ImageShadow: React.FC<Props> = ({width, height, children}) => {
+export const ImageShadow: React.FC = () => {
   return (
-    <Svg height={height ?? '100%'} width={width ?? '100%'}>
+    <Svg width="100%" height="100%" viewBox="0 0 400 225">
       <Defs>
         <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
           <Stop offset="0" stopColor="transparent" stopOpacity="0" />
-          <Stop offset="1" stopColor="black" stopOpacity="0.7" />
+          <Stop offset="1" stopColor="black" stopOpacity="0.9" />
         </LinearGradient>
-        {children}
       </Defs>
       <Rect
         x="0"
         y="0"
-        width={width ?? '100%'}
-        height={height ?? '100%'}
+        width="100%"
+        height="100%"
         fill="url(#grad)"
         rx="5"
         ry="5"
