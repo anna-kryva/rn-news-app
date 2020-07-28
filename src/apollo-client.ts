f001669bcdeed774039f6b836f340bcee58eaf50
Anna Kryva <anna120100@gmail.com>
2020-07-28 12:54:03 +0300

    Adjusted configuration and folder structure following code review notes

---
+++ src/apollo-client.ts
@@ -0,0 +1,22 @@
+import {ApolloClient, InMemoryCache} from '@apollo/client';
+import Constants from 'expo-constants';
+
+const client = new ApolloClient({
+  uri: Constants.manifest.extra.graphCMSUrl,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          article(_, {args, toReference}) {
+            return toReference({
+              __typename: 'Article',
+              id: args?.where.id ?? null,
+            });
+          },
+        },
+      },
+    },
+  }),
+});
+
+export default client;
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,12 @@
 import {ArticlesQuery} from './generated/graphql';
-import {StackScreenProps} from '@react-navigation/stack';
 
-export type ArticleCardType = ArticlesQuery['articles'][number];
+export type ArticleType = ArticlesQuery['articles'][number];
 
-export type TagType = ArticleCardType['tags'][number];
+export type TagType = ArticleType['tags'][number];
 
-export type CoverImage = ArticleCardType['cover'];
+export type ArticleCoverType = ArticleType['cover'];
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Main: undefined;
   Article: {id: string};
 };
-
-export type ArticleScreenProps = StackScreenProps<
-  RootStackParamList,
-  'Article'
->;
-
-export type MainScreenProps = StackScreenProps<RootStackParamList, 'Main'>;
